edd2f9309c4b2ba2a5d2655df7f9d7d113a64f7c
GGMachina <ggonza55@uic.edu>
2023-02-28 23:31:52 -0600

    Successfully returns usr input on back btn

--- app/src/main/java/edu/uic/cs478/s2023/projectone/MainActivity.java
+++ app/src/main/java/edu/uic/cs478/s2023/projectone/MainActivity.java
@@ -1,15 +1,19 @@
 package edu.uic.cs478.s2023.projectone;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Button btnOne;
     Button btnTwo;
+
+    String validPhoneNum = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,8 +25,11 @@ public class MainActivity extends AppCompatActivity {
         btnOne.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
+                int SECOND_ACTIVITY_CODE = 1;
                 Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                startActivity(intent);
+                startActivityForResult(intent, SECOND_ACTIVITY_CODE);
+
+
             }
         });
 
@@ -33,5 +40,31 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d("ActivityReturned", "requestCode: " + requestCode + " resultCode: " + resultCode);
+        String receivedNum = "";
+        if (data.getExtras().containsKey("phoneInfo")) {
+            receivedNum = data.getStringExtra("phoneInfo");
+        } else {
+            throw new RuntimeException("Returned from Phone Number Activity but failed to retrieve phone number...");
+        }
+        Log.d("ActivityReturned", "onActivityResult: " + receivedNum);
+
+        if (isValidPhoneNum(receivedNum)) {
+
+            //TODO: Enable the 2nd button, their number is valid, they can now use the dialer
+        } else {
+            //TODO: Keep dialer button disabled, but give toast notifying them of invalidity.
+        }
+    }
+    public boolean isValidPhoneNum(String givenNum) {
+//        TODO: Check the validity of givenNum
+//        Must be exactly 10 numbers.
+//        Can have parenthesis, if so, can also have space, but MUST have dash.
+        return false;
     }
 }
--- app/src/main/java/edu/uic/cs478/s2023/projectone/SecondActivity.java
+++ app/src/main/java/edu/uic/cs478/s2023/projectone/SecondActivity.java
@@ -1,15 +1,39 @@
 package edu.uic.cs478.s2023.projectone;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
-
+    Button btnReturn;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        btnReturn = findViewById(R.id.btnReturn);
+
+        btnReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+//        super.onBackPressed();
+        EditText numField = findViewById(R.id.editTextPhoneNum);
+        String phoneNum = numField.getText().toString();
+        Intent intentPhone = getIntent();
+        intentPhone.putExtra("phoneInfo", phoneNum);
+        setResult(RESULT_OK, intentPhone);
+        finish();
     }
 }
