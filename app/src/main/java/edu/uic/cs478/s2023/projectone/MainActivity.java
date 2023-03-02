ca0e6a999c14685277bcb7b8e236bf3d90d88d11
GGMachina <ggonza55@uic.edu>
2023-03-01 18:02:05 -0600

    Finished project, all functionality implemented.

--- app/src/main/java/edu/uic/cs478/s2023/projectone/MainActivity.java
+++ app/src/main/java/edu/uic/cs478/s2023/projectone/MainActivity.java
@@ -1,19 +1,46 @@
+//////////////////////////////////////////////////////////////////////////////////
+// Author: Geovani Gonzalez
+// Name:   Project One
+// Class:  CS 478 Spring 23
+// Date:   March 1st, 2023
+// Desc:   This is the main activity file for Project One. Application takes number
+// from user and then checks if it is in a valid format. If so, it is given to the
+// dialer app of the phone's choice, and put in, without dialing.
+//////////////////////////////////////////////////////////////////////////////////
+// Notes:
+//
+// 1. The PDF requires use of the setOnEditorActionListener, as seen in the
+// SecondActivity file. This has been done, however I feel the need to point out,
+// this means the user's input is ONLY captured when they press enter. This seems
+// inferior to the version I first went with, where user input is captured with
+// the use of onBackPressed, so even if they don't press enter it is saved.
+// These two methods are of course incompatible, so the one in the PDF has been kept.
+//
+// 2. The editText field gives a numeric input keypad, but this prevents only the
+// use of alphabetic characters. You can still type in '(', ')', '-', etc.
+//////////////////////////////////////////////////////////////////////////////////
+
 package edu.uic.cs478.s2023.projectone;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     Button btnOne;
     Button btnTwo;
 
-    String validPhoneNum = "";
+    String returnedPhoneNum = "";
+    boolean isValidPhoneNum = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,8 +56,15 @@ public class MainActivity extends AppCompatActivity {
         });
 
         btnTwo.setOnClickListener(v -> {
-            Intent intent = new Intent(Intent.ACTION_DIAL);
-            startActivity(intent);
+            if (isValidPhoneNum) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:" + returnedPhoneNum));
+                startActivity(intent);
+            } else {
+                Toast.makeText(this, "'" + (Objects.equals(returnedPhoneNum, "") ? "[No number provided]" : returnedPhoneNum)+ "' is not in a valid format. Please try again!", Toast.LENGTH_LONG).show();
+            }
+
+
         });
     }
 
@@ -38,14 +72,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         Log.d("ActivityReturned", "requestCode: " + requestCode + " resultCode: " + resultCode);
-        if (resultCode == RESULT_OK) {
-            if (data != null && data.getExtras().containsKey("phoneInfo")) {
-                validPhoneNum = data.getStringExtra("phoneInfo");
-            } else {
-                throw new RuntimeException("Returned from Phone Number Activity but failed to retrieve phone number...");
-            }
+        if (data != null) {
+            returnedPhoneNum = data.getStringExtra("phoneInfo");
+        } else {
+            throw new RuntimeException("Returned from Phone Number Activity but failed to retrieve phone number, data was null...");
         }
+        isValidPhoneNum = resultCode == RESULT_OK;
 
-        Log.d("ActivityReturned", "onActivityResult: " + validPhoneNum);
+        Log.d("ActivityReturned", "onActivityResult: " + returnedPhoneNum);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("heldNum", returnedPhoneNum);
+        outState.putBoolean("isValid", isValidPhoneNum);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        returnedPhoneNum = savedInstanceState.getString("heldNum", null);
+        isValidPhoneNum = savedInstanceState.getBoolean("isValid", false);
     }
 }
--- app/src/main/java/edu/uic/cs478/s2023/projectone/SecondActivity.java
+++ app/src/main/java/edu/uic/cs478/s2023/projectone/SecondActivity.java
@@ -2,41 +2,124 @@ package edu.uic.cs478.s2023.projectone;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
     Button btnReturn;
+    String currentNum = "";
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
         btnReturn = findViewById(R.id.btnReturn);
-
         btnReturn.setOnClickListener(view -> onBackPressed());
+        EditText numField = findViewById(R.id.editTextPhoneNum);
+        numField.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                currentNum = textView.getText().toString();
+                Log.d("EditTriggered", "onEditorAction: textview now says " + currentNum);
+                return true;
+
+            }
+        });
     }
 
     @Override
     public void onBackPressed() {
-//        super.onBackPressed();
-        EditText numField = findViewById(R.id.editTextPhoneNum);
-        String phoneNum = numField.getText().toString();
+//        EditText numField = findViewById(R.id.editTextPhoneNum);
+//        String phoneNum = numField.getText().toString();
         Intent intentPhone = getIntent();
-        intentPhone.putExtra("phoneInfo", phoneNum);
-        if (isValidPhoneNum(phoneNum)) {
+        intentPhone.putExtra("phoneInfo", currentNum);
+        if (isValidPhoneNum(currentNum)) {
             setResult(RESULT_OK, intentPhone);
         } else {
-            setResult(RESULT_CANCELED);
+            setResult(RESULT_CANCELED, intentPhone);
         }
         finish();
     }
 
     public boolean isValidPhoneNum(String givenNum) {
+//        DONE: Implement validation process
+//        1112223333 - 10 characters long
+//        (111)222-3333 - 13 characters long
+//        (111) 222-3333 - 14 characters long
+//        Only these 3 possibilities
+        if (givenNum.length() == 10 && isStringNumeric(givenNum)) {
+            return true;
+        }
+        else if (givenNum.length() > 12 && isFormattedPhoneNum(givenNum)) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isNumericChar(char c) {
+        return (int) c >= 48 && (int) c <= 57;
+    }
+    private boolean isStringNumeric (String s) {
+        for (int i = 0; i < s.length(); i++) {
+            if (!isNumericChar(s.charAt(i))) {
+                return false;
+            }
+        }
         return true;
+    }
+    private boolean isFormattedPhoneNum(String s) {
+        String copyS = s;
+//        (111)222- 3  3  3  3 - Phone Number of 13 length
+//        012345678 9 10 11 12 - Indexes
+//        (111) 222- 3  3  3  3 - Phone Number of 14 length
+//        0123456789 10 11 12 13 - Indexes
+//        Check Area code first (***)
+        if ((int)s.charAt(0) == 40 && (int)s.charAt(4) == 41) {
+            copyS = copyS.substring(1,4) + copyS.substring(5);
+        }
+//        Should now be 111 222-3333 or 111222-3333
+        String[] strings = copyS.split(" ");
+//        We should have ["111222-3333"] or ["111", "222-3333"]
+        if (strings.length == 1) {
+//            if 111222-3333
+            String[] lastStrings = strings[0].split("-");
+//            Should be ["111222", "3333"]
+            if ((lastStrings[0]).length() == 6 && lastStrings[1].length() == 4 && isStringNumeric(lastStrings[0]) && isStringNumeric(lastStrings[1])) {
+                return true; //This was a 13 character long formatted number!
+            }
+        } else if (strings.length == 2) {
+//            if 111 222-3333
+            String[] lastStrings = strings[1].split("-");
+//            should be ["222", "3333"]
+            if (strings[0].length() == 3
+                && lastStrings[0].length() == 3
+                && lastStrings[1].length() == 4
+                && isStringNumeric(strings[0])
+                && isStringNumeric(lastStrings[0])
+                && isStringNumeric(lastStrings[1])
+            ) {
+                return true; //This was a 14 character long formatted number!
+            }
+        }
+        return false;
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("curNum", currentNum);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        currentNum = savedInstanceState.getString("curNum", null);
     }
 }
